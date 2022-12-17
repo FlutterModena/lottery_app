e7f662df9cd8b8f9a07cd0e721f96cce47aff5cd
Carmine Zaccagnino <carmine@carminezacc.com>
2022-12-17 18:08:14 +0100

    live coding

---
+++ android/app/src/main/kotlin/com/example/estrazione_libro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estrazione_libro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  List<String> names = [];
+  String currentName = "";
+  var random = Random();
+  String? winnerName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        currentName = value;
+                      },
+                      decoration:
+                          const InputDecoration(hintText: "Nome persona"),
+                    ),
+                  ),
+                  IconButton(
+                      iconSize: 24,
+                      onPressed: () {
+                        setState(() {
+                          names.add(currentName);
+                        });
+                      },
+                      icon: const Icon(Icons.send))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: names.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(names[index]),
+                  );
+                },
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                var n = random.nextInt(names.length);
+
+                setState(() {
+                  winnerName = names[n];
+                });
+              },
+              child: const Text("Estrai un nome"),
+            ),
+            if (winnerName != null)
+              Wrap(children: [
+                Text(
+                  "The winner is $winnerName",
+                  style: const TextStyle(fontSize: 30),
+                ),
+              ])
+          ],
+        ),
+      ),
+    );
+  }
+}
